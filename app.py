3857d7cb7aa2bb10319df80064e48e29ac9b9799
Ali Jan <alijan.rahujo143@gmail.com>
2026-06-22 12:29:23 +0500

    Lec 4

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ def about():
 @app.route('/contact')
 def ali():
     return render_template('contact.html')
-    
+
+# @app.route('/lec4')
+# def lec4():
+#     return render_template('lec 4.html')
+
+# @app.route('/lec4/<name>')
+# def lec4(name):
+
+#     return render_template('lec 4.html',name=name)   
+
+# @app.route('/lec4/<username>')
+# def lec4(username):
+#     return render_template('lec 4.html',name=username) 
+
+@app.route('/lec4/<username>/<table>')
+def lec4(username,table):
+    return render_template('lec 4.html',name=username,table=table)   
 
 if __name__ == "__main__":
     app.run(debug=True)
